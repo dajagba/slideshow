2a4a38101b66bc3cfb9e6f768773e02166d47470
Dominic Ajagba <dominicajagba@Dominics-MBP.attlocal.net>
2022-05-21 22:38:09 -0400

    fix to repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { environment } from '../environments/environment'; // Angular CLI enviro
     StoreModule.forRoot(appReducers),EffectsModule.forRoot([SlideShowEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
+      logOnly: environment.production,  // Restrict extension to log-only mode
     }),
   ],
   providers: [],
